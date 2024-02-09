261bf64e5e1036cef128fb54cca403262632fb26
David Wright <davidwright7793@gmail.com>
2024-02-09 18:44:03 -0500

    added search button and search input and started geocode function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 var APIkey = "978c80bc59731a55915d21f7ea8bc0d1"
+
+var searchBtn = document.querySelector("#search-button")
+
+searchBtn.addEventListener("click", function(){
+    var searchInput = document.querySelector("#search-value").value
+    console.log(searchInput)
+
+    // use searchInput to fulfill cityname parameter in geoCode
+    //geoCode(searchInput)
+})
+
+function geoCode(cityname){
+fetch (``)
+.then(response => response.json())
+.then(weatherData => {
+    console.log(weatherData)
+    
+})
+}
